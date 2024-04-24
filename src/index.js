1da838c128b8448014f4ef321a7aab860664a17d
Santiago Jair Torres Rivera <st147086@gmail.com>
2024-04-23 23:37:13 -0500

    Pequeños cambios

--- src/index.js
+++ src/index.js
@@ -5,16 +5,18 @@ const puerto = 3000;
 const asesoriaRoutes = require("./routes/asesoria");
 const mongoose = require("mongoose");
 
+require('dotenv').config();
 app.use(parser.urlencoded({extended: false}));
 app.use(parser.json());
 
 app.use("/api", asesoriaRoutes);
 app.use(express.json());
-
-mongoose.connect(process.env.MONGODB_URI)
+// console.log(process.env.MONGODB_URI);
+mongoose
+    .connect(process.env.MONGODB_URI)
     .then(() => console.log("Conexión exitosa."))
     .catch((error) => console.log(error));
 
 app.listen(puerto, () => {
     console.log(`Aplicacion corriendo en el puerto ${puerto}`);
-})
+});
